643601d6d3b96164f8beaafc8aaf45b4d1e54083
Matheus Franzener Hohmann <matheus_hohmann@estudante.sc.senai.br>
2022-11-10 18:27:41 -0300

    10/11 - 18:27
    Projeto back-end maven

---
+++ Back-End/tutankhamuns-casino/src/main/java/br/senai/sc/tutankhamunscasino/TutankhamunsCasinoApplication.java
@@ -0,0 +1,13 @@
+package br.senai.sc.tutankhamunscasino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutankhamunsCasinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutankhamunsCasinoApplication.class, args);
+	}
+
+}
---
+++ Back-End/tutankhamuns-casino/src/test/java/br/senai/sc/tutankhamunscasino/TutankhamunsCasinoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.senai.sc.tutankhamunscasino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutankhamunsCasinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
