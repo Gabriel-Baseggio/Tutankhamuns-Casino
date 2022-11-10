de171439b48d6d07ba34181297ac7564b8efd8f7
JoaoVitorWinter <joao_v_winter@estudante.sc.senai.br>
2022-11-10 09:22:27 -0300

    Criação da página login

---
+++ js/login.js
@@ -0,0 +1,11 @@
+document.getElementById('olho').addEventListener('click', function () {
+    document.getElementById('inputSenha').type = 'text';
+    document.getElementById('olho').hidden = "true";
+    document.getElementById('olho2').removeAttribute('hidden');
+});
+
+document.getElementById('olho2').addEventListener('click', function () {
+    document.getElementById('inputSenha').type = 'password';
+    document.getElementById('olho2').hidden = "true";
+    document.getElementById('olho').removeAttribute('hidden');
+});
