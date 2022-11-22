69a4c69dfd5893bf47f0161fc9106c4a2ad0fa4b
Gabriel-Baseggio <baseggiogabriel6@gmail.com>
2022-11-22 10:55:01 -0300

    Todas as páginas criadas

---
+++ Front-End/js/home.js
@@ -0,0 +1,11 @@
+function ranking(){
+    window.location.href = `ranking.html`
+}
+
+function perfil(){
+    window.location.href = `perfil.html`
+}
+
+function cacaNiquel(){
+    window.location.href = `cacaniquel.html`
+}
